76d7105c20b161e54f7c9f360861d4c02d138aa7
Jan Mandel <jan.mandel@gmail.com>
2022-03-30 15:11:59 -0600

    time

--- alderaan_single_gpu.sh
+++ alderaan_single_gpu.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# alderaan_single.sh
-# A simple single core job template
+# A simple single core job template with gpu
 # Jan Mandel, March 2022 
 #SBATCH --job-name=gpu
 #SBATCH --partition=math-alderaan-gpu
-#SBATCH --time=1:00:00                    # Max wall-clock time
+#SBATCH --time=1-1:00:00                    # Max wall-clock time - one day one hour
 #SBATCH --ntasks=1                        # number of cores 
 
 # run tensorflow in singularity container
