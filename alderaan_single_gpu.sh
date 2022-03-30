9323983f6ae6581ecb93c3a4f686c7f9230f4a9d
Jan Mandel <jan.mandel@gmail.com>
2022-03-30 15:00:34 -0600

    comments, change job name

--- alderaan_single_gpu.sh
+++ alderaan_single_gpu.sh
@@ -2,9 +2,13 @@
 # alderaan_single.sh
 # A simple single core job template
 # Jan Mandel, March 2022 
-#SBATCH --job-name=mpi_hello_single
+#SBATCH --job-name=gpu
 #SBATCH --partition=math-alderaan-gpu
 #SBATCH --time=1:00:00                    # Max wall-clock time
 #SBATCH --ntasks=1                        # number of cores 
 
-singularity exec /storage/singularity/tensorflow.sif python3 gpucode.py >& gpucode.log
+# run tensorflow in singularity container
+# redirect output to a file so that it can be inspected before the end of the job
+singularity exec /storage/singularity/tensorflow.sif python3 gpucode.py >& gpucode.log 
+# copy the output to the job output for reference
+cat gpucode.log
