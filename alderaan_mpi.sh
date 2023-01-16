0220535711820e235c723082acd33aea23de822f
Jan Mandel <jan.mandel@gmail.com>
2023-01-15 21:33:09 -0700

    adding mpmd example

--- alderaan_mpi.sh
+++ alderaan_mpi.sh
@@ -5,7 +5,10 @@
 #SBATCH --job-name=mpi_hello
 #SBATCH --partition=math-alderaan
 #SBATCH --time=1:00:00                    # Max wall-clock time
-#SBATCH --ntasks=360                      # Total number of MPI processes, no need for --nodes
+#SBATCH --ntasks=128                      # Total number of MPI processes, no need for --nodes
 
-mpirun examples/mpi_hello_world.exe       # replace by your own executable, no need for -np
+mpirun examples/mpi_hello_world.exe       # replace by your own executable, no need for -np, uses --ntasks above
 
+echo MPMD example
+
+mpirun -n 5 examples/mpi_hello_world.exe : -n 10 examples/mpi_hello_world.exe # replace by your own executable, no need for -np
