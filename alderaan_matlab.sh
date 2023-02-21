3f2373bc85690c90f122ebd54fa58970bba0cf93
Jan Mandel <jan.mandel@gmail.com>
2023-02-20 22:40:33 -0700

    renamed:    matlab_alderaan.slurm -> alderaan_matlab.sh 	modified:   alderaan_single_gpu.sh

--- alderaan_matlab.sh
+++ alderaan_matlab.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=singularity
+#SBATCH --job-name=cwmatlab
 #SBATCH --partition=math-alderaan
 #SBATCH --nodes=1                   # Number of requested nodes
 #SBATCH --time=1:00:00              # Max wall-clock time
 # matlab can use multiple cores
-#SBATCH --ntasks=16                 # Total number of tasks over all nodes, max 64*nodes
+#SBATCH --ntasks=4                 # Total number of tasks over all nodes, max 64*nodes
 
 # prepare yuor matlat script, here it is script.m
 cat test.m |  matlab -nodisplay -nosplash -nodesktop
