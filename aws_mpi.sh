723e7cc259030859fa2789a57676afff33fec7e4
Jan Mandel <jan.mandel@gmail.com>
2023-06-21 04:14:48 +0000

    adding aws tests

--- aws_mpi.sh
+++ aws_mpi.sh
@@ -5,11 +5,15 @@
 #SBATCH --job-name=mpi_hello
 #SBATCH --partition=compute
 #SBATCH --time=1:00:00                    # Max wall-clock time
-#SBATCH --ntasks=180                      # Total number of MPI processe
-#SBATCH --nodes=10
+#SBATCH --ntasks=384                      # Total number of MPI processe
+#SBATCH --nodes=2
 
-mpirun examples/mpi_hello_world.exe       # replace by your own executable, no need for -np, uses --ntasks above
-
-echo MPMD example
-
-mpirun -n 5 examples/mpi_hello_world.exe : -n 10 examples/mpi_hello_world.exe # replace by your own executable, no need for -np
+which mpirun
+log=aws_mpi.log
+rm -f $log
+date > $log
+mpirun examples/mpi_hello_world.exe >> $log
+mpirun examples/mpi/pt2pt/osu_bw >>  $log
+mpirun examples/mpi/pt2pt/osu_latency >> $log
+date >> $log
+echo DONE >> $log
