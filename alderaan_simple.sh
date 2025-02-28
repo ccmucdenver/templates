756f5ef628233cf933a8f138dd03090feb977035
Jan Mandel <jan.mandel@gmail.com>
2025-02-27 18:04:28 -0700

    Use queue math-alderaan-short modified:   alderaan_simple.sh

--- alderaan_simple.sh
+++ alderaan_simple.sh
@@ -3,8 +3,10 @@
 # A simple single core job template
 # Jan Mandel, December 2021
 #SBATCH --job-name=mpi_hello_single
-#SBATCH --partition=math-alderaan
+#SBATCH --partition=math-alderaan-short
 #SBATCH --time=1:00:00                    # Max wall-clock time
 #SBATCH --ntasks=1                        # number of cores, leave at 1 
 
+sleep 60
+env > alderaan_single.$SLURM_JOB_ID 
 echo hello from node `hostname`
